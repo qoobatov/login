72d7489b25c5da81f08d1387736edeb29b5f0576
Erkin Kubatov <qoobatov@gmail.com>
2023-03-02 15:37:12 +0600

    created tabs functions

--- script.js
+++ script.js
@@ -34,11 +34,58 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // ********************************* tabs *******************************************************
 
+  const btns = document.querySelectorAll(".button"),
+    tabsContent = document.querySelectorAll(".tab-content"),
+    btnsParent = document.querySelector(".tab-btns-block"),
+    resetLinkParent = document.querySelector(".reset-password-parent"),
+    resetBtnParent = document.querySelector(".reset-btns-block"),
+    addedMargin = document.querySelector(".container-login");
 
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.style.display = "none";
+    });
+  }
 
+  function showTabContent(i) {
+    tabsContent[i].style.display = "block";
+    if (i != 1) {
+      addedMargin.style.marginTop = "70px";
+    } else {
+      addedMargin.style.marginTop = "0px";
+    }
+  }
 
+  hideTabContent();
+  showTabContent(0);
 
+  btnsParent.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target && target.classList.contains("button")) {
+      btns.forEach((item, i) => {
+        if (target === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
 
+  resetLinkParent.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target && target.classList.contains("forgot-password")) {
+      hideTabContent();
+      showTabContent(2);
+    }
+  });
 
-  
+  resetBtnParent.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target && target.classList.contains("btn-reset-cancel")) {
+      hideTabContent();
+      showTabContent(0);
+    }
+  });
+
+  // ************************************ end tabs ******************************************************
 });
