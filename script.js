ec8478808806ae2d7a972515f321281620982d99
Erkin Kubatov <qoobatov@gmail.com>
2023-03-02 10:08:43 +0600

    added function modal window

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-$("#register-form input[name='']")
+window.addEventListener("DOMContentLoaded", () => {
+  // start modal window for login ***************************************
+
+  const modalTrigger = document.querySelector("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+  }
+
+  modalTrigger.addEventListener("click", openModal);
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+    modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  // если че для Esc
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  });
+});
